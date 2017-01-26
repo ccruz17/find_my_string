233b5574080473787731d1511f9f1f21a925dc4c
Christian Cruz <ccruz.ga17@gmail.com>
2017-01-26 15:06:45 -0600

    Tdo: Prepare for composer and test

---
+++ src/Finder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class Csv_reader, you can read and excel file
+ *
+ * This class can extract information from a target or multiple targets and each of them with one or more values
+ *
+ * @category   CCRUZ Find my string
+ * @package    ccruz17/find_my_string
+ * @author     Christian Cruz Garrido <ccruz.ga17@gmail.com>
+ * @copyright  2016-2017 CCRUZ
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt  GNU GENERAL PUBLIC LICENSE V3
+ * @link       https://github.com/ccruz17/find_my_string
+ */
+
+namespace ccruz17;
+
+class Finder
+{
+    public function find_my_strings() {
+        echo 'Test Composer';
+    }
+}
